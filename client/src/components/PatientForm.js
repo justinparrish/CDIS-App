77e31153d08efa870f11f17c8587a0a785085d87
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-03-24 17:35:05 -0400

    creating directory for additional components

---
+++ client/src/components/PatientForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PatientForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
