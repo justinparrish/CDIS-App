e0d16e4d1c4864524ef3581774618c846adb296a
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-03-25 15:01:24 -0400

    added functionality to handle inputs and when submitted

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -11,28 +11,42 @@ export default class ReviewForm extends Component {
         labs: '',
         type: ''
     }
+
+    handleInputs = (evnt) => {
+        let state = { ...this.state }
+
+        state[evnt.target.name] = evnt.target.value
+
+        this.setState(state)
+    }
+    handleSubmit = (evnt) => {
+        evnt.preventDefault()
+
+        console.log(this.state)
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <fieldset>
                     <label>Ep</label>
-                    <input type='text' name='ep' placeholder='500 Words Max' />
+                    <input type='text' name='ep' placeholder='500 Words Max' onChange={this.handleInputs} />
                     <label>Vital Signs</label>
-                    <input type='text' name='vitalSigns' placeholder='500 Words Max' />
+                    <input type='text' name='vitalSigns' placeholder='500 Words Max' onChange={this.handleInputs} />
                     <label>Diagnostics</label>
-                    <input type='text' name='diagnostic' placeholder='500 Words Max' />
+                    <input type='text' name='diagnostic' placeholder='500 Words Max' onChange={this.handleInputs} />
                     <label>Mar</label>
-                    <input type='text' name='mar' placeholder='500 Words Max' />
+                    <input type='text' name='mar' placeholder='500 Words Max' onChange={this.handleInputs} />
                     <label>H&P</label>
-                    <input type='text' name='historyAndPhysical' placeholder='500 Words Max' />
+                    <input type='text' name='historyAndPhysical' placeholder='500 Words Max' onChange={this.handleInputs} />
                     <label>Query Opportunites</label>
-                    <input type='text' name='queryOpp' placeholder='500 Words Max' />
+                    <input type='text' name='queryOpp' placeholder='500 Words Max' onChange={this.handleInputs} />
                 </fieldset>
                 <fieldset>
                     <label>Labs</label>
-                    <input type='text' name='labs' placeholder='500 Words Max' />
+                    <input type='text' name='labs' placeholder='500 Words Max' onChange={this.handleInputs} />
                     <label>Type</label>
-                    <input type='text' name='type' placeholder='Review or Re-Review' />
+                    <input type='text' name='type' placeholder='Review or Re-Review' onChange={this.handleInputs} />
                     <input type='submit' value='Add' />
                 </fieldset>
             </form>
