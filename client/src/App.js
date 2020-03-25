2394e8e7946e7b37626414f287a5d0c487f91cb0
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-03-25 14:43:08 -0400

    created sample component for form for reviews

--- client/src/App.js
+++ client/src/App.js
@@ -162,7 +162,31 @@ const patientReviews = [
 
 ]
 
-
+const reviewForm = () => (
+  <form>
+    <fieldset>
+    <label>Ep</label>
+    <input type='text' name='ep' placeholder='500 Words Max'/>
+    <label>Vital Signs</label>
+    <input type='text' name='vitalSigns' placeholder='500 Words Max'/>
+    <label>Diagnostics</label>
+    <input type='text' name='diagnostic' placeholder='500 Words Max'/>
+    <label>Mar</label>
+    <input type='text' name='mar' placeholder='500 Words Max'/>
+    <label>H&P</label>
+    <input type='text' name='historyAndPhysical' placeholder='500 Words Max'/>
+    <label>Query Opportunites</label>
+    <input type='text' name='queryOpp' placeholder='500 Words Max'/>
+    </fieldset>
+    <fieldset>
+    <label>Labs</label>
+    <input type='text' name='labs' placeholder='500 Words Max'/>
+    <label>Type</label>
+    <input type='text' name='type' placeholder='Review or Re-Review'/>
+    <input type='submit' value='Add' />
+    </fieldset>
+  </form>
+)
 
 class App extends React.Component {
   addNewPatient = (info) => {
@@ -184,6 +208,7 @@ class App extends React.Component {
         <QueryForm addNewQuery={this.addNewQuery}/>
         {queryList(patientQueries)}
         <h3>Review List</h3>
+        {reviewForm()}
         {reviewList(patientReviews)}
       </div>
     )
