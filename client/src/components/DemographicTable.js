b1e354972e6f4077012af8f28f743625ce631c74
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-03-31 16:13:44 -0400

    basic table shows with each patient listed

--- client/src/components/DemographicTable.js
+++ client/src/components/DemographicTable.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 import { Menu, Segment, Table } from 'semantic-ui-react'
 
-const tableRow = (demographic) => (
+const tableRow = (patient) => (
     <Table.Row>
-        <Table.Cell>{demographic.patientName}</Table.Cell>
-        <Table.Cell>{demographic.age}</Table.Cell>
-        <Table.Cell>{demographic.dob}</Table.Cell>
-        <Table.Cell selectable><a href='#'>Select</a>Select</Table.Cell>
+        <Table.Cell>{patient.demographic.patientName}</Table.Cell>
+        <Table.Cell>{patient.demographic.age}</Table.Cell>
+        <Table.Cell>{patient.demographic.dob}</Table.Cell>
+        <Table.Cell selectable><a href='#'>Select</a></Table.Cell>
     </Table.Row>
 )
 
+const patientList = (patients) => (
+    patients.map(tableRow)
+)
 
-
+const employeePatients = (employee) => (
+    patientList(employee.patients)
+)
 
 export default class DemographicTable extends Component {
     render() {
@@ -33,6 +38,7 @@ export default class DemographicTable extends Component {
                             <Table.Cell>1972-03-12</Table.Cell>
                             <Table.Cell selectable><a href='#'>Select</a></Table.Cell>
                         </Table.Row>
+                        {employeePatients(this.props.currentEmployee)}
                     </Table.Body>
                 </Table>
             </div>
