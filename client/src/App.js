76183714132d821c45ecace93af969969599d2e4
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-04-01 19:14:35 -0400

    added functionality to  helper function for patientForm to add a patient's data structure

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const getPatientInfo = (patient) => (
 )
 
 const listPatientInfo = (patients) => (
-<div>{patients.map(getPatientInfo)}</div>
+  <div>{patients.map(getPatientInfo)}</div>
 )
 
 const employeePatients = (employee) => (
@@ -110,120 +110,120 @@ const employeeList = (employees, currentEmployee, onChange) => (
 )
 
 // ----------- Test Data Structure --------------
-const docs = 
-  {
-    1: {
-      id: 1,
-      name: 'Tenia Parrish',
-      username: 'tenia64',
-      password: 'tenia71',
-      email: 'tenia64@msn.com',
-      patients: [
-        {
+const docs =
+{
+  1: {
+    id: 1,
+    name: 'Tenia Parrish',
+    username: 'tenia64',
+    password: 'tenia71',
+    email: 'tenia64@msn.com',
+    patients: [
+      {
+        id: 1,
+        accountNum: 'EUH6304393485',
+        medicalRecNum: 'EUH694321',
+        admitDate: '2020-16-02',
+        lengthOfStay: 3,
+        financialClass: 'Medicare',
+        status: 'Discharged',
+        demographic: {
           id: 1,
-          accountNum: 'EUH6304393485',
-          medicalRecNum: 'EUH694321',
-          admitDate: '2020-16-02',
-          lengthOfStay: 3,
-          financialClass: 'Medicare',
-          status: 'Discharged',
-          demographic: {
-            id: 1,
-            patientName: 'Nelson, Justin D',
-            age: 20,
-            dob: '2000-10-03'
-          },
-          room: {
-            id: 1,
-            nursingUnit: 'T5-S',
-            room: 'T509-01',
-            dateIn: '2020-16-02',
-            dateOut: '2020-19-02',
-          },
-          query: [
-            {
-              id: 1,
-              doctorName: 'Morgan, Phil R., NP',
-              doctorQuestion: "Doctor's Question number 1",
-              clinicalIndicators: "Pt admitted with Hypercapnia Respitory Failure with ALS",
-              historyAndPhysical: "history and physical 1",
-              treatment: "3/4/20 Normal Saline 60ml Tube flush given x 6",
-            }
-          ],
-          review: [
-            {
-              id: 1,
-              ep: 'ep 1',
-              vitalSigns: "vital signs 1",
-              diagnostic: "diagnostic 1",
-              mar: "medication administration record 1",
-              historyAndPhysical: "history and physical 1",
-              queryOpp: "query opportunities 1",
-              labs: "lab results 1",
-              type: "Review"
-            }
-          ]
+          patientName: 'Nelson, Justin D',
+          age: 20,
+          dob: '2000-10-03'
         },
-        
-      ]
-    },
-    2: {
-      id: 2,
-      name: 'Justin Parrish',
-      username: 'Justin12',
-      password: 'justin2112',
-      email: 'justin@msn.com',
-      patients: [
-        {
+        room: {
           id: 1,
-          accountNum: 'EUH6304393485',
-          medicalRecNum: 'EUH694321',
-          admitDate: '2020-16-02',
-          lengthOfStay: 3,
-          financialClass: 'Medicare',
-          status: 'Discharged',
-          demographic: {
+          nursingUnit: 'T5-S',
+          room: 'T509-01',
+          dateIn: '2020-16-02',
+          dateOut: '2020-19-02',
+        },
+        query: [
+          {
             id: 1,
-            patientName: 'Nelson, D',
-            age: 10,
-            dob: '2010-10-03'
-          },
-          room: {
+            doctorName: 'Morgan, Phil R., NP',
+            doctorQuestion: "Doctor's Question number 1",
+            clinicalIndicators: "Pt admitted with Hypercapnia Respitory Failure with ALS",
+            historyAndPhysical: "history and physical 1",
+            treatment: "3/4/20 Normal Saline 60ml Tube flush given x 6",
+          }
+        ],
+        review: [
+          {
             id: 1,
-            nursingUnit: 'T4-S',
-            room: 'T411-01',
-            dateIn: '2020-11-02',
-            dateOut: '2020-11-02',
-          },
-          query: [
-            {
-              id: 1,
-              doctorName: 'Morgan, R., NP',
-              doctorQuestion: "Doctor's Quen number 1",
-              clinicalIndicators: "Pt ted with Hypercapnia Respitory Failure with ALS",
-              historyAndPhysical: "ory and physical 1",
-              treatment: "3rmal Saline 60ml Tube flush given x 6",
-            }
-          ],
-          review: [
-            {
-              id: 1,
-              ep: 'ep 1',
-              vitalSigns: "val signs 1",
-              diagnostic: "dgnostic 1",
-              mar: "medicn administration record 1",
-              historyAndPhysical: "his and physical 1",
-              queryOpp: "query opnities 1",
-              labs: "lab resu1",
-              type: "Review"
-            }
-          ]
-        }
-      ]
-    }
+            ep: 'ep 1',
+            vitalSigns: "vital signs 1",
+            diagnostic: "diagnostic 1",
+            mar: "medication administration record 1",
+            historyAndPhysical: "history and physical 1",
+            queryOpp: "query opportunities 1",
+            labs: "lab results 1",
+            type: "Review"
+          }
+        ]
+      },
+
+    ]
+  },
+  2: {
+    id: 2,
+    name: 'Justin Parrish',
+    username: 'Justin12',
+    password: 'justin2112',
+    email: 'justin@msn.com',
+    patients: [
+      {
+        id: 1,
+        accountNum: 'EUH6304393485',
+        medicalRecNum: 'EUH694321',
+        admitDate: '2020-16-02',
+        lengthOfStay: 3,
+        financialClass: 'Medicare',
+        status: 'Discharged',
+        demographic: {
+          id: 1,
+          patientName: 'Nelson, D',
+          age: 10,
+          dob: '2010-10-03'
+        },
+        room: {
+          id: 1,
+          nursingUnit: 'T4-S',
+          room: 'T411-01',
+          dateIn: '2020-11-02',
+          dateOut: '2020-11-02',
+        },
+        query: [
+          {
+            id: 1,
+            doctorName: 'Morgan, R., NP',
+            doctorQuestion: "Doctor's Quen number 1",
+            clinicalIndicators: "Pt ted with Hypercapnia Respitory Failure with ALS",
+            historyAndPhysical: "ory and physical 1",
+            treatment: "3rmal Saline 60ml Tube flush given x 6",
+          }
+        ],
+        review: [
+          {
+            id: 1,
+            ep: 'ep 1',
+            vitalSigns: "val signs 1",
+            diagnostic: "dgnostic 1",
+            mar: "medicn administration record 1",
+            historyAndPhysical: "his and physical 1",
+            queryOpp: "query opnities 1",
+            labs: "lab resu1",
+            type: "Review"
+          }
+        ]
+      }
+    ]
   }
-  
-  
+}
+
+
 class App extends React.Component {
   state = {
     employees: docs,
@@ -235,16 +235,51 @@ class App extends React.Component {
   )
 
   setCurrentEmployee = (currentEmployee) => {
-    this.setState({currentEmployee})
+    this.setState({ currentEmployee })
   }
 
   getCurrentEmployee = () => (
     this.state.employees[this.state.currentEmployee]
   )
-
+  // ------------------------------------------
   addNewPatient = (info) => {
     console.log("Patient From App Comp.", info)
+    let state = this.state
+
+    
+    let newPatientInfo = {
+      id: 2,
+      accountNum: info.accountNum,
+      medicalRecNum: info.medicalRecNum,
+      admitDate: info.admitDate,
+      lengthOfStay: info.lengthOfStay,
+      financialClass: info.financialClass,
+      status: info.status,
+    }
+    console.log("Patient Info part 1", newPatientInfo)
+    let newDemographic = {
+      id: 2,
+      patientName: info.patientName,
+      age: info.age,
+      dob: info.dob
+    }
+    let newRoom = {
+      id: 2,
+      nursingUnit: info.nursingUnit,
+      room: info.room,
+      dateIn: info.dateIn,
+      dateOut: info.dateOut
+    }
+    newPatientInfo.demographic = newDemographic
+    newPatientInfo.room = newRoom
+    newPatientInfo.query = []
+    newPatientInfo.review= []
+    console.log("Demographic", newDemographic)
+    console.log("Room Info", newRoom)
+    console.log("Patient Info part 2", newPatientInfo)
+
   }
+  // ------------------------------------------
   addNewQuery = (info) => {
     console.log("Query From App Comp.", info)
     let state = { ...this.state }
@@ -269,8 +304,9 @@ class App extends React.Component {
         {employeePatients(this.getCurrentEmployee())}
         <h3>Patient Demographics</h3>
         {patientsDemographic(this.getCurrentEmployee())}
-        <DemographicTable currentEmployee={this.getCurrentEmployee()}/>
-        <Routes />
+        <PatientForm addNewPatient={this.addNewPatient} />
+        <DemographicTable currentEmployee={this.getCurrentEmployee()} />
+
       </div>
     )
   }
