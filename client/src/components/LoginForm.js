ab5139d3ce8b916e7426af494fe926c5ddd5d92a
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-04-04 11:24:30 -0400

    can handle form inputs and submit on form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 
 export default class LoginForm extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleInputs = (evnt) => {
+        let state = {...this.state}
+
+        state[evnt.target.name] = evnt.target.value
+
+        this.setState(state)
+    }
+    handleSubmit = (evnt) => {
+        evnt.preventDefault()
+
+        this.props.authenicate(this.state)
+    }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>Username</label>
-                <input type='text' name='username' />
+                <input type='text' name='username' onChange={this.handleInputs}/>
                 <label>Password</label>
-                <input type='password' name='password' />
+                <input type='password' name='password' onChange={this.handleInputs}/>
                 <input type='submit' value='Submit' />
             </form>
         )
