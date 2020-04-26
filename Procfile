1c5fa0e5841290243ee713152d9fa4e3859ee0ef
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-04-26 13:27:36 -0400

    heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn cdis.wsgi --log-file -
--- cdis/settings.py
+++ cdis/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 
@@ -128,3 +130,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+django_heroku.settings(locals())
