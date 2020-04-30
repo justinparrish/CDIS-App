014db29c2950730d7ac2b7c1c1524aabea8021e5
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-04-30 11:54:41 -0400

    working on heroku deployment of frontend

--- cdis/settings.py
+++ cdis/settings.py
@@ -131,7 +131,7 @@ REST_FRAMEWORK = {
 REACT_APP_DIR = "client"
 
 STATICFILES_DIRS = [
-    os.path.join(REACT_APP_DIR, 'build', 'static')
+    os.path.join( 'build', 'static')
 ]
 
 django_heroku.settings(locals())
