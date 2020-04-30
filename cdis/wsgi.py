60151df8c44771c56e7d4a5568e4dcf75c1a2874
justinparrish7@gmail.com <justinparrish7@gmail.com>
2020-04-30 15:42:31 -0400

    updating setting to base deployment setting

--- cdis/wsgi.py
+++ cdis/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for cdis project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
@@ -11,9 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cdis.settings')
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
